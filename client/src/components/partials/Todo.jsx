ef2c7a668e38f0ec68344926ac1682dcad346c06
harshraoka <81157043+harshraoka@users.noreply.github.com>
2023-07-10 21:44:07 +0530

    Update Todo.jsx

--- client/src/components/partials/Todo.jsx
+++ client/src/components/partials/Todo.jsx
@@ -34,7 +34,7 @@ function Todo({todo,setrefreshList}) {
   return (
     <div className='col-sm-3 mx-3 my-2 alert bg-light'>
         <div className="card-header">
-            {todo.isCompleted ? ' Completed': 'Not Completed' }
+            {!todo.isCompleted ? ' Completed': 'Not Completed' }
         </div>
         <div className="card-body">
             <h4 className='card-title' style={{textDecoration: !todo.isCompleted ? 'line-through':'none' ,color:'black'}}>{todo.desc}</h4>
@@ -44,7 +44,7 @@ function Todo({todo,setrefreshList}) {
           <div className="actionBUttons" style={{display:'flex', justifyContent:'space-between'}}>
               <button style={{background:'red'}} onClick={handleDelete} >Delete</button>
               <div className="markTodo">
-              <button onClick={handleMark} >{todo.isCompleted ? 'Mark Uncompleted' : 'Mark Completed'}</button>
+              <button onClick={handleMark} >{!todo.isCompleted ? 'Mark Uncompleted' : 'Mark Completed'}</button>
             </div>
             </div>
           </div>
